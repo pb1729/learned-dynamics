3075a5f32238e2009ccbbd1ed3def5d8ca42e5ca
Phillip Bement <phillipb@pbement.com>
2024-06-06 17:55:28 -0700

    try a bunch more wgan architectures. add vector and scalar GroupNorm implementations for graph-based data.

--- layers_common.py
+++ layers_common.py
@@ -250,7 +250,7 @@ class EdgeRelativeEmbed(nn.Module):
         return: tuple(a_out, v_out)
         a_out: (batch, edge, adim)
         v_out: (batch, edge, vdim, 3) """
-    vecs = torch.stack([ # 6 = 4 choose 2 relative vectors
+    vecs = torch.stack([ # 6 = (4 choose 2) relative vectors
         pos_0[:, graph.dst] - pos_0[:, graph.src],
         pos_1[:, graph.dst] - pos_1[:, graph.src],
         pos_1[:, graph.src] - pos_0[:, graph.src],
@@ -276,12 +276,118 @@ class NodeRelativeEmbed(nn.Module):
         return: tuple(a_out, v_out)
         a_out: (batch, edge, adim)
         v_out: (batch, edge, vdim, 3) """
-    vecs = torch.stack([ # 1 = 2 choose 2 relative vectors
+    vecs = torch.stack([ # 1 = (2 choose 2) relative vectors
         pos_1 - pos_0,
       ], dim=2) # (batch, edges, 1, 3)
     norms = torch.linalg.vector_norm(vecs, dim=-1) # (batch, edges, 1)
     a_out = self.lin_a(norms)
     v_out = self.lin_v(vecs)/3 # fudge factor of 1/3
     return a_out, v_out
+
+
+class EdgeRelativeEmbedMLP(nn.Module):
+  """ input embedding for edges, where 2 positions are passed as input """
+  def __init__(self, adim, vdim):
+    super().__init__()
+    self.lin_v = VecLinear(6, vdim)
+    self.scalar_layers = nn.Sequential(
+      nn.Linear(6, adim),
+      nn.LeakyReLU(0.2, inplace=True),
+      nn.Linear(adim, adim),
+      nn.LeakyReLU(0.2, inplace=True),
+      nn.Linear(adim, adim))
+  def forward(self, pos_0, pos_1, graph):
+    """ pos0: (batch, node, 3)
+        pos1: (batch, node, 3)
+        return: tuple(a_out, v_out)
+        a_out: (batch, edge, adim)
+        v_out: (batch, edge, vdim, 3) """
+    vecs = torch.stack([ # 6 = (4 choose 2) relative vectors
+        pos_0[:, graph.dst] - pos_0[:, graph.src],
+        pos_1[:, graph.dst] - pos_1[:, graph.src],
+        pos_1[:, graph.src] - pos_0[:, graph.src],
+        pos_1[:, graph.dst] - pos_0[:, graph.dst],
+        pos_1[:, graph.src] - pos_0[:, graph.dst],
+        pos_1[:, graph.dst] - pos_0[:, graph.src],
+      ], dim=2) # (batch, edges, 6, 3)
+    norms = torch.linalg.vector_norm(vecs, dim=-1) # (batch, edges, 6)
+    a_out = self.scalar_layers(norms)
+    v_out = self.lin_v(vecs)/3 # fudge factor of 1/3
+    return a_out, v_out
+
+
+class NodeRelativeEmbedMLP(nn.Module):
+  """ input embedding for nodes, where 2 positions are passed as input """
+  def __init__(self, adim, vdim):
+    super().__init__()
+    self.lin_v = VecLinear(1, vdim)
+    self.scalar_layers = nn.Sequential(
+      nn.Linear(1, adim),
+      nn.LeakyReLU(0.2, inplace=True),
+      nn.Linear(adim, adim),
+      nn.LeakyReLU(0.2, inplace=True),
+      nn.Linear(adim, adim))
+  def forward(self, pos_0, pos_1, graph):
+    """ pos0: (batch, node, 3)
+        pos1: (batch, node, 3)
+        return: tuple(a_out, v_out)
+        a_out: (batch, edge, adim)
+        v_out: (batch, edge, vdim, 3) """
+    vecs = torch.stack([ # 1 = (2 choose 2) relative vectors
+        pos_1 - pos_0,
+      ], dim=2) # (batch, edges, 1, 3)
+    norms = torch.linalg.vector_norm(vecs, dim=-1) # (batch, edges, 1)
+    a_out = self.scalar_layers(norms)
+    v_out = self.lin_v(vecs)/3 # fudge factor of 1/3
+    return a_out, v_out
+
+
+class ScalGroupNorm(nn.Module):
+  """ Scalar group norm, should be a fairly standard group norm as found eg here:
+      https://pytorch.org/docs/stable/generated/torch.nn.GroupNorm.html
+      We average over the nodes (or edges) of the graph as well as each group. """
+  def __init__(self, chan, groups, epsilon=1e-5):
+    super().__init__()
+    assert chan % groups == 0
+    self.chan = chan
+    self.groups = groups
+    self.epsilon = epsilon
+    self.gamma = nn.Parameter(torch.ones(chan))
+    self.beta = nn.Parameter(torch.zeros(chan))
+  def forward(self, x):
+    """ note: where we write "nodes" here, we could also write "edges"
+        x: (batch, nodes, chan)
+        return: (batch, nodes, chan) """
+    batch, nodes, must_be[self.chan] = x.shape
+    x = x.reshape(batch, nodes, self.groups, -1)
+    mean = x.mean([1, 3], keepdim=True)
+    x_shift = x - mean
+    var = ((x_shift)**2).mean([1, 3], keepdim=True)
+    ans = x_shift/torch.sqrt(self.epsilon + var)
+    return self.beta + self.gamma*ans.reshape(batch, nodes, self.chan)
+
+class VecGroupNorm(nn.Module):
+  def __init__(self, chan, groups, epsilon=1e-5):
+    super().__init__()
+    assert chan % groups == 0
+    self.chan = chan
+    self.groups = groups
+    self.epsilon = epsilon
+    self.gamma = nn.Parameter(torch.ones(chan, 1))
+  def forward(self, x):
+    """ note: where we write "nodes" here, we could also write "edges"
+        x: (batch, nodes, chan, 3)
+        return: (batch, nodes, chan, 3) """
+    batch, nodes, must_be[self.chan], must_be[3] = x.shape
+    x = x.reshape(batch, nodes, self.groups, -1, 3)
+    moment2 = (x**2).sum(4, keepdim=True).mean([1, 3], keepdim=True) # (batch, 1, groups, 1, 1)
+    ans = x/torch.sqrt(self.epsilon + moment2)
+    return self.gamma*ans.reshape(batch, nodes, self.chan, 3)
+
+
+
+
+
+
 
 
--- training_scripts/train_3d.py
+++ training_scripts/train_3d.py
@@ -7,18 +7,19 @@ from config import Config, Condition
 from train import training_run
 
 
-SIMTYPE = "3d_ou_poly"
-ARCH = "wgan_3d_simple"
-RUN_ID = "B"
+SIMTYPE = "3d_quart_ou_poly"
+ARCH = "wgan_3d_simple4"
+RUN_ID = "I"
 
-L_LIST = [12, 24, 36, 48]
+L_LIST = [2, 5, 12, 24]#[12, 24, 36, 48]
 T_LIST = [3, 10, 30, 100]
 
 arch_specific = {
-  "lr_d": 0.0008, "lr_g": 0.0003,
+  "lr_d": 0.001, "lr_g": 0.00005, "lpen_wt": 1.0,
   "beta_1": 0.5, "beta_2": 0.99,
-  "z_scale": 10.0, "inst_noise_str_r": 0.3, "inst_noise_str_g": 0.2,
-  "adim": 32, "vdim": 16, "rank": 8,
+  "z_scale": 7.0, "inst_noise_str_r": 0.1, "inst_noise_str_g": 0.,
+  "adim": 64, "vdim": 32, "agroups": 8, "vgroups": 4,
+  "rank": 16,
 }
 
 for l in L_LIST:
@@ -28,7 +29,7 @@ for l in L_LIST:
       Config(sim_name, ARCH,
         cond=Condition.COORDS, x_only=True,
         batch=8, simlen=6, t_eql=4,
-        nsteps=2048, save_every=512,
+        nsteps=65536, save_every=512,
         arch_specific=arch_specific))
 
 
