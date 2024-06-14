d5f8c4d89bae1d492a46878023b4c42eb2307928
Phillip Bement <phillipb@pbement.com>
2024-06-14 15:39:35 -0700

    add two architectures: wgan_3d_1dconv_2 tries to do a second denoising round in the generator, to see if that improves things at all. it doesn't seem to help much. the other architecture is wgan_3d_taxicab, which appears to result in quite a big improvement! this architecture basically differs by moving a single parenthesis in the code, but the associated mathematical change is non-trivial. we must use the L2 distance to define distances between atoms, nothing else is rotationally invariant. but we have a free choice in what metric we use to aggregate all the atom displacements into a single overall distance. up until now, we've been using the L2 metric for that too. but the taxicab architecture uses the taxicab metric! so to get the overall distance between configurations, we just add up the amounts each atom has been displaced by, which is quite intuitive.

--- training_scripts/train_3d.py
+++ training_scripts/train_3d.py
@@ -8,16 +8,16 @@ from train import training_run
 
 
 SIMTYPE = "3d_quart_ou_poly"
-ARCH = "wgan_3d_1dconv"
-RUN_ID = "K2"
+ARCH = "wgan_3d_taxicab"
+RUN_ID = "L2"
 
-L_LIST = [2, 5, 12, 24]#[12, 24, 36, 48]
-T_LIST = [3, 100]#[3, 10, 30, 100]
+L_LIST = [12]#[2, 5, 12, 24, 36, 48]
+T_LIST = [100]#[3, 10, 30, 100]
 
 arch_specific = {
   "lr_d": 0.0008, "lr_g": 0.00005, "lpen_wt": 1.0,
   "beta_1": 0.5, "beta_2": 0.99, "weight_decay": 0.01,
-  "z_scale": 40.0, "inst_noise_str_r": 0., "inst_noise_str_g": 0.,
+  "z_scale": 18., "inst_noise_str_r": 0., "inst_noise_str_g": 0.,
   "adim": 64, "vdim": 32, "agroups": 8, "vgroups": 4,
   "rank": 24,
 }
