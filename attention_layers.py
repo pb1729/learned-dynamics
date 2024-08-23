5822e5c2862f99fc0508e625bdfa0fd38767e098
Phillip Bement <phillipb@pbement.com>
2024-08-22 17:25:23 -0700

    add a version 2 of proximity attention that incorporates fourier encoding of the radial separation between atoms

--- attention_layers.py
+++ attention_layers.py
@@ -120,6 +120,103 @@ class ProximityAttention(nn.Module):
 
 
 
+class ProximityAttentionV2(nn.Module):
+  """ A multiheaded attention module. The layer can be configured with the following parameters:
+      r0_list: (H)        --> a python list of characteristic radii for each attention head
+      kq_dim: (dim, dim)  --> tuple of adim and vdim for keys and queries
+      chan: (dim, dim)    --> tuple of adim and vdim for input and output data
+      kq_prescale: ()     --> k*q dot products are multiplied by this to make them smaller
+      Attention is based both on vector dot products, and also on spatial proximity.
+      This implementation does not allow masking.
+      
+      This is version 2, with radial separation encodings added onto the attention operation. """
+  def __init__(self, r0_list, kq_dim, chan, kq_prescale=0.01, kq_scale=0.1):
+    super().__init__()
+    self.H = len(r0_list)
+    self.register_buffer("r0", torch.tensor(r0_list), persistent=False)
+    kq_adim, kq_vdim = kq_dim
+    achan, vchan = chan
+    self.kq_ascale = (kq_prescale/kq_adim)**0.5
+    self.kq_vscale = (kq_prescale/kq_vdim)**0.5
+    self.kq_scale = kq_scale
+    assert achan % self.H == 0, "channels must be evenly divisible by number of heads"
+    assert vchan % self.H == 0, "channels must be evenly divisible by number of heads"
+    self.W_ak = nn.Parameter(torch.empty(self.H, kq_adim, achan))
+    self.W_vk = nn.Parameter(torch.empty(self.H, kq_vdim, vchan))
+    self.W_aq = nn.Parameter(torch.empty(self.H, kq_adim, achan))
+    self.W_vq = nn.Parameter(torch.empty(self.H, kq_vdim, vchan))
+    avaldim, vvaldim = achan // self.H, vchan // self.H # divide by number of heads to get useful sizes for value weight matrices
+    self.W_aval = nn.Parameter(torch.empty(self.H, avaldim, achan))
+    self.W_vval = nn.Parameter(torch.empty(self.H, vvaldim, vchan))
+    self.W_a2vval = nn.Parameter(torch.empty(self.H, vvaldim, achan)) # special weight matrix for getting displacement info
+    self.W_v2aval = nn.Parameter(torch.empty(self.H, avaldim, vchan)) # special weight matrix for getting displacement info
+    self.W_aradenc = nn.Parameter(torch.empty(self.H, avaldim, 16)) # special weight matrix for radial encodings
+    self.W_vradenc = nn.Parameter(torch.empty(self.H, vvaldim, 16)) # special weight matrix for radial encodings
+  def _param_init_with_scale(self, param, scale=1.):
+    must_be[self.H], fan_in, fan_out = param.shape
+    std = scale*(fan_in**(-0.5))
+    nn.init.normal_(param, std=std)
+  def self_init(self):
+    self._param_init_with_scale(self.W_ak, self.kq_ascale)
+    self._param_init_with_scale(self.W_vk, self.kq_vscale)
+    self._param_init_with_scale(self.W_aq, self.kq_ascale)
+    self._param_init_with_scale(self.W_vq, self.kq_vscale)
+    self._param_init_with_scale(self.W_aval)
+    self._param_init_with_scale(self.W_vval)
+    self._param_init_with_scale(self.W_a2vval)
+    self._param_init_with_scale(self.W_v2aval)
+    self._param_init_with_scale(self.W_aradenc)
+    self._param_init_with_scale(self.W_vradenc)
+  def forward(self, x, pos_k, pos_q):
+    """ performs a proximity attention operation.
+        x is the input data vector
+        pos_k, pos_q are the spatial locations of the key and query probe points
+        x: tuple(ax, vx)
+          ax: (batch, nodes, achan)
+          vx: (batch, nodes, vchan, 3)
+        pos_k, pos_q: (batch, nodes, 3)
+        Attention score is given by softmax(k*q)*(r0**2 / (r0**2 + r**2))
+        Data about separations between probe points is mixed into the output.
+        ans: tuple(aans, vans)
+          aans: (batch, nodes, achan)
+          vans: (batch, nodes, vchan, 3) """
+    ax, vx = x
+    batch,          nodes,          achan = ax.shape
+    must_be[batch], must_be[nodes], vchan, must_be[3] = vx.shape
+    must_be[batch], must_be[nodes], must_be[3] = pos_k.shape
+    must_be[batch], must_be[nodes], must_be[3] = pos_q.shape
+    # compute basic attention:
+    ak = torch.einsum("hij, bnj -> bnhi",   self.W_ak, ax) # (batch, nodes, H, kq_adim)
+    vk = torch.einsum("hij, bnjv -> bnhiv", self.W_vk, vx) # (batch, nodes, H, kq_vdim, 3)
+    aq = torch.einsum("hij, bnj -> bnhi",   self.W_aq, ax) # (batch, nodes, H, kq_adim)
+    vq = torch.einsum("hij, bnjv -> bnhiv", self.W_vq, vx) # (batch, nodes, H, kq_vdim, 3)
+    akq_dot = torch.einsum("bnhi, bmhi -> bnmh",   ak, aq) # (batch, nodes, nodes, H)
+    vkq_dot = torch.einsum("bnhiv, bmhiv -> bnmh", vk, vq) # (batch, nodes, nodes, H)
+    dot = self.kq_scale*(akq_dot + vkq_dot) # (batch, nodes, nodes, H)
+    # compute proximity:
+    separation = pos_k[:, None, :] - pos_q[:, :, None] # (batch, nodes, nodes, 3)
+    dist_sq = (separation**2).sum(3) # (batch, nodes, nodes) squared-distance matrix
+    r0_sq = self.r0**2
+    proximity = r0_sq/(r0_sq + dist_sq[..., None]) # (batch, nodes, nodes, H)
+    directions = separation[:, :, :, None]*torch.sqrt(proximity/r0_sq)[:, :, :, :, None] # (batch, nodes, nodes, H, 3) max magnitude is 1
+    dot = dot + torch.log(proximity) # update attention pre-activations with proximity
+    # attention operation
+    attention = torch.softmax(dot, dim=1) # dim 1 corresponds to "which key?"
+    aans = torch.einsum("bnmh, hij, bnj -> bmhi",   attention, self.W_aval, ax) # (batch, nodes, H, avaldim)
+    vans = torch.einsum("bnmh, hij, bnjv -> bmhiv", attention, self.W_vval, vx) # (batch, nodes, H, vvaldim, 3)
+    # special value messages using spatial displacements:
+    aans = aans + torch.einsum("bnmh, hij, bnjv, bnmhv -> bmhi", attention, self.W_v2aval, vx, directions)
+    vans = vans + torch.einsum("bnmh, hij, bnj, bnmhv -> bmhiv", attention, self.W_a2vval, ax, directions)
+    # version 2 addition: radial encodings!
+    relative_dist = torch.sqrt(dist_sq[..., None]/r0_sq)[..., None] # (batch, nodes, nodes, H, 1)
+    ω = torch.arange(8, device=relative_dist.device)*0.5
+    radenc = torch.cat([torch.sin(ω*relative_dist), torch.cos(ω*relative_dist)], dim=-1) # (batch, nodes, nodes, H, 16)
+    aans = aans + torch.einsum("bnmh, hir, bnmhr -> bmhi", attention, self.W_aradenc, radenc) # (batch, nodes, H, avaldim)
+    vans = vans + torch.einsum("bnmh, hir, bnmhr, bnmhv -> bmhiv", attention, self.W_vradenc, radenc, directions)
+    return aans.reshape(batch, nodes, achan), vans.reshape(batch, nodes, vchan, 3)
+
+
+
 
 if __name__ == "__main__":
   device = "cuda"
@@ -130,7 +227,8 @@ if __name__ == "__main__":
   pp = ProbePoints(2, 2, adim, vdim)
   pp.self_init()
   pp.to(device)
-  m = ProximityAttention([0.5, 0.75, 1., 1.5, 2., 3., 4., 6., 8., 12., 16., 24., 32., 48.], (16, 12), (88, 44))
+  # m = ProximityAttention([0.5, 0.75, 1., 1.5, 2., 3., 4., 6., 8., 12., 16.], (16, 12), (88, 44))
+  m = ProximityAttentionV2([0.5, 0.75, 1., 1.5, 2., 3., 4., 6., 8., 12., 16.], (16, 12), (88, 44))
   m.self_init()
   m.to(device)
   ax = torch.randn(batch, nodes, adim, device=device)
