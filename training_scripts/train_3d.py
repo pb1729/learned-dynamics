d5f1f22d610e27d5b64d197e59a2ecab6ba6af00
Phillip Bement <phillipb@pbement.com>
2024-10-08 18:00:45 -0700

    add another light weight architecture, wgan_3d_flash4. as with the other light-weight archs, we run into stability issues. not sure why these differ from the previous class of archs, but there are of course some slight differences in how the attention layers work, etc. also, local residual no longer has an activation function on vectors (TODO: if this is indeed the problem, should we try doing something about it?). anyways, it turns out that reducing the learning rate does appear to solve this. huh. in the current code, lr_d is reduced by 5 and lr_g by slightly less, but we should see how close we can cut it, I guess.

--- training_scripts/train_3d.py
+++ training_scripts/train_3d.py
@@ -7,28 +7,29 @@ from config import Config
 from train import training_run
 
 
-SIMTYPE = "repel5"
-ARCH = "wgan_3d_flash2"
-RUN_ID = "Q6"
+SIMTYPE = "repel5a"
+ARCH = "wgan_3d_flash4"
+RUN_ID = "Q10"
 
-L_LIST = [80] # [48]
-T_LIST = [20]#[10, 15, 20, 30, 40, 60, 80, 120, 160]
-NSTEPS_LIST = [1024, 2048, 3072, 4096, 5120, 6144, 7168, 8192, 9216, 10240, 11264, 12288, 13312, 14336, 15360, 16384, 17408, 18432, 19456, 20480, 21504, 22528, 23552, 24576, 25600, 26624, 27648, 28672, 29696, 30720, 31744, 32768]
+L_LIST = [80]#[48] # [80]
+# rouse time for repel5 and repel5a is 317
+T_LIST = [round(31.7*i) for i in range(1, 11)]
+NSTEPS_LIST = [1024, 2048, 3072, 4096, 5120, 6144, 7168, 8192, 9216, 10240, 11264, 12288, 13312, 14336, 15360, 16384]
 
 
 
 arch_specific = {
-  "lr_d": 0.001,  "lr_d_fac": 0.98,
-  "lr_g": 0.0001, "lr_g_fac": 0.95,
+  "lr_d": 0.0002,  "lr_d_fac": 0.98,
+  "lr_g": 0.00004, "lr_g_fac": 0.95,
   "lpen_wt": 1.0,
   "beta_1": 0., "beta_2": 0.99, "weight_decay": 0.001,
   "z_scale": 20.,
   "adim": 64, "vdim": 32, "agroups": 8, "vgroups": 4,
   "rank": 24,
   "gp_coeff": 1.,
-  "hinge": True, "hinge_leak": 0.1,
+  "hinge": True, "hinge_leak": 0.0,
   # multihead stuff
-  "covar_pen": False, # seems to actually *create* instability these days
+  "covar_pen": False, # seems to actually *create* instability these days (this is just due to y_g numbers being large, then taken to 4th power)
   "heads": 8, # probably don't need this?
   # interval stuff
   "ndiscs": 4,
