a853d9f9309eecbe8d0712771378116220bb203d
Phillip Bement <phillipb@pbement.com>
2025-01-20 11:25:45 -0800

    add the newblock11 architecture. only difference is that generator output has normalization of the gradient tacked on

--- training_scripts/train_openmm.py
+++ training_scripts/train_openmm.py
@@ -6,9 +6,9 @@ from managan.config import Config
 from managan.train import training_run
 
 
-SIMTYPE = "A_t2000_L40_m20_M45"
-ARCH = "wgan_3d_newblock10_res"
-RUN_ID = "T4"
+SIMTYPE = "A_t20_L40_m20_M25"#"A_t2000_L40_m20_M45"
+ARCH = "wgan_3d_newblock11_res"
+RUN_ID = "T5"
 
 NSTEPS_LIST = [512*i for i in range(1, 33)]
 
@@ -16,7 +16,7 @@ NSTEPS_LIST = [512*i for i in range(1, 33)]
 sim_name = SIMTYPE
 training_run("models/%s_%s.%s.pt" % (RUN_ID, sim_name, ARCH),
   Config("openmm:" + sim_name, ARCH,
-    batch=1, simlen=16, t_eql=4,
+    batch=1, simlen=16,
     nsteps=NSTEPS_LIST, save_every=128,
     arch_specific={
       "lr_d": 0.0002,  "lr_d_fac": 0.98,
@@ -29,7 +29,7 @@ training_run("models/%s_%s.%s.pt" % (RUN_ID, sim_name, ARCH),
       "hinge": True, "hinge_leak": 0.1,
       # multihead stuff
       "covar_pen": False, # seems to actually *create* instability these days
-      "heads": 8, # probably don't need this?
+      "heads": 1, # probably don't need this?
       # interval stuff
       "ndiscs": 2,#4,
       # ACE stuff
