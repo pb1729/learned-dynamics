f25578ce963f69241fd4a376a390a2d723be1771
Phillip Bement <phillipb@pbement.com>
2025-03-21 11:20:32 -0700

    fix test_ramachandran so that angles line up on the same amino acid instead of being offset by 1

--- test_ramachandran.py
+++ test_ramachandran.py
@@ -9,7 +9,11 @@ from plotting_common import approx_squarish_factorize
 
 
 def get_angles(traj):
-  """ return Ramachandran phis and psis for a ModelState trajectory """
+  """ return Ramachandran phis and psis for a ModelState trajectory
+      VERY IMPORTANT NOTE: For a chain of N amino acids, we'll get N-1 of each angle.
+      psi: exists for aminos in range(0, N-1)
+      phi: exists for aminos in range(1, N)
+      So there are N-2 aminos with defined values of both angles, range(1, N-1). """
   mdtrajecs = model_states_to_mdtrajs(traj)
   phis = np.stack([
       mdtraj.compute_phi(mdtrajec)[1]
@@ -24,7 +28,10 @@ def get_angles(traj):
 def density_plot(angles):
   for prednm in angles:
     phis, psis = angles[prednm]
-    plt.scatter(phis.flatten(), psis.flatten(), label=prednm)
+    # cut off amino acids on the ends with only one angle:
+    phis = phis[..., :-1]
+    psis = psis[..., 1:]
+    plt.scatter(phis.flatten(), psis.flatten(), marker=".", label=prednm)
   plt.legend()
   plt.xlim(-np.pi, np.pi)
   plt.ylim(-np.pi, np.pi)
@@ -63,8 +70,8 @@ def tcorr_plot(angles):
   for i in range(npep):
     ax_phi = axs_flat[2*i]
     ax_psi = axs_flat[2*i + 1]
-    ax_phi.set_ylabel(f"bond {i}: <cos(φ(0) - φ(t))>")
-    ax_psi.set_ylabel(f"bond {i}: <cos(ψ(0) - ψ(t))>")
+    ax_phi.set_ylabel(f"amino {i + 1}: <cos(φ(0) - φ(t))>")
+    ax_psi.set_ylabel(f"amino {i}: <cos(ψ(0) - ψ(t))>")
     for ax in [ax_phi, ax_psi]:
       ax.set_xlabel("time [steps]")
       ax.legend()
