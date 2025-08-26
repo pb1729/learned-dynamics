34dbd1817101818cb0d96f376d040cf9d9707ddf
Phillip Bement <phillipb@pbement.com>
2025-08-25 17:00:42 -0700

    create FastSegmentedPolynomialFromUniform1dJit layer. this is meant to be faster (and a little more limited) than the out-of-the-box implementation from cuequivariance_ops_torch; in terms of CPU overhead

--- managan/cueq_tensor_products.py
+++ managan/cueq_tensor_products.py
@@ -5,15 +5,17 @@ import cuequivariance_torch as cueq_torch
 
 from .utils import must_be
 from .tp_construction_cueq import make_elementwise_tensor_product
+from .fast_tp_unif_1d_jit import FastSegmentedPolynomialFromUniform1dJit
 
 
-def _flatten_vec(x):
-  """ x: (..., 3, chan)
-      ans: (..., 3*chan) """
-  *rest, must_be[3], chan = x.shape
-  return x.reshape(*rest, 3*chan)
+def _flatten_irrep(irrep_l:int, x):
+  """ x: (..., irrep_dim, chan)
+      ans: (..., irrep_dim*chan) """
+  irrep_dim = 2*irrep_l + 1
+  *rest, must_be[irrep_dim], chan = x.shape
+  return x.reshape(*rest, irrep_dim*chan)
 
-def _transpose_irrep_muls_to_channels(rank, num_irreps, irrep_l, x):
+def _transpose_irrep_muls_to_channels(rank:int, num_irreps:int, irrep_l:int, x):
   """ Converts tensor x with irreps `{rank}x{l}`*num_irreps in ir_mul order to `{rank*num_irreps}x{x}` in ir_mul order. """
   irrep_dim = 2*irrep_l + 1
   *rest, must_be[rank*num_irreps*irrep_dim] = x.shape
@@ -22,37 +24,55 @@ def _transpose_irrep_muls_to_channels(rank, num_irreps, irrep_l, x):
   x = x.reshape(*rest, irrep_dim, num_irreps*rank)
   return x
 
+class ElementwiseTensorProd_o0(nn.Module):
+  def __init__(self, rank):
+    super().__init__()
+    poly_o0, irreps_o0 = make_elementwise_tensor_product(rank, [(0, 0, 0), (1, 1, 0)])
+    self.tens_prod_o0 = FastSegmentedPolynomialFromUniform1dJit(poly_o0, name="elementwise_o0")
+  def forward(self, z_0_l, z_1_l, z_0_r, z_1_r):
+    z_l = torch.cat([z_0_l, _flatten_irrep(1, z_1_l)], dim=-1)
+    z_r = torch.cat([z_0_r, _flatten_irrep(1, z_1_r)], dim=-1)
+    z_0_o, = self.tens_prod_o0([z_l, z_r]) # pattern-match because output is a list
+    return z_0_o
+
+class ElementwiseTensorProd_o1(nn.Module):
+  def __init__(self, rank):
+    super().__init__()
+    poly_o1, irreps_o1 = make_elementwise_tensor_product(rank, [(0, 1, 1), (1, 0, 1), (1, 1, 1)])
+    self.tens_prod_o1 = FastSegmentedPolynomialFromUniform1dJit(poly_o1, name="elementwise_o1")
+    self.rank = rank
+    self.num_o1_irreps = len(irreps_o1)
+  def forward(self, z_0_l, z_1_l, z_0_r, z_1_r):
+    z_l = torch.cat([z_0_l, _flatten_irrep(1, z_1_l)], dim=-1)
+    z_r = torch.cat([z_0_r, _flatten_irrep(1, z_1_r)], dim=-1)
+    z_1_o, = self.tens_prod_o1([z_l, z_r]) # pattern-match because output is a list
+    return _transpose_irrep_muls_to_channels(self.rank, self.num_o1_irreps, 1, z_1_o)
+
+
 class ElementwiseTensorProducts(nn.Module):
   def __init__(self, rank, chan):
     super().__init__()
-    poly_o0, irreps_o0 = make_elementwise_tensor_product(rank, [(0, 0, 0), (1, 1, 0)])
-    poly_o1, irreps_o1 = make_elementwise_tensor_product(rank, [(0, 1, 1), (1, 0, 1), (1, 1, 1)])
     self.lin_0_l = nn.Linear(chan, rank)
     self.lin_0_r = nn.Linear(chan, rank)
     self.lin_1_l = nn.Linear(chan, rank, bias=False)
     self.lin_1_r = nn.Linear(chan, rank, bias=False)
-    self.lin_0_o = nn.Linear(irreps_o0.count("0"), chan)
-    self.lin_1_o = nn.Linear(irreps_o1.count("1"), chan, bias=False)
-    self.tens_prod_o0 = cueq_torch.SegmentedPolynomial(poly_o0, name="elementwise_o0")
-    self.tens_prod_o1 = cueq_torch.SegmentedPolynomial(poly_o1, name="elementwise_o1")
-    self.rank = rank
-    self.num_o1_irreps = len(irreps_o1)
+    self.lin_0_o = nn.Linear(2*rank, chan)              # 2 kinds of products make scalars
+    self.lin_1_o = nn.Linear(3*rank, chan, bias=False)  # 3 kinds of products make vectors
+    self.etp_o0 = ElementwiseTensorProd_o0(rank)
+    self.etp_o1 = ElementwiseTensorProd_o1(rank)
   def forward(self, z0, z1):
     z0_l = self.lin_0_l(z0)
     z0_r = self.lin_0_r(z0)
     z1_l = self.lin_1_l(z1)
     z1_r = self.lin_1_r(z1)
-    z_l = torch.cat([z0_l, _flatten_vec(z1_l)], -1)
-    z_r = torch.cat([z0_r, _flatten_vec(z1_r)], -1)
-    z0_o, = self.tens_prod_o0([z_l, z_r]) # pattern-match because output is a list
-    z1_o, = self.tens_prod_o1([z_l, z_r]) # pattern-match because output is a list
-    z1_o = _transpose_irrep_muls_to_channels(self.rank, self.num_o1_irreps, 1, z1_o)
+    z0_o = self.etp_o0(z0_l, z1_l, z0_r, z1_r)
+    z1_o = self.etp_o1(z0_l, z1_l, z0_r, z1_r)
     ans = self.lin_0_o(z0_o), self.lin_1_o(z1_o)
     return ans
 
 
 class MessageContract(nn.Module):
-  def __init__(self, rank, chan, dist_emb_dim):
+  def __init__(self, rank, chan, dist_emb_dim, actv_mod=nn.SiLU):
     super().__init__()
     poly_o0, irreps_o0 = make_elementwise_tensor_product(rank, [(0, 0, 0), (1, 1, 0)], r_irreps=[0, 1, 2])
     poly_o1, irreps_o1 = make_elementwise_tensor_product(rank, [(0, 1, 1), (1, 0, 1), (1, 1, 1), (1, 2, 1)], r_irreps=[0, 1, 2])
@@ -60,15 +80,15 @@ class MessageContract(nn.Module):
     self.lin_1_actv = nn.Linear(chan, rank, bias=False)
     self.dist_mlp = nn.Sequential(
       nn.Linear(dist_emb_dim, rank),
-      nn.LeakyReLU(0.1),
+      actv_mod(),
       nn.Linear(rank, rank),
-      nn.LeakyReLU(0.1),
+      actv_mod(),
       nn.Linear(rank, rank))
     self.lin_0_o = nn.Linear(irreps_o0.count("0"), chan)
     self.lin_1_o = nn.Linear(irreps_o1.count("1"), chan, bias=False)
     self.sh = cueq_torch.SphericalHarmonics([0, 1, 2], normalize=False)
-    self.tens_prod_o0 = cueq_torch.SegmentedPolynomial(poly_o0, name="messages_o0")
-    self.tens_prod_o1 = cueq_torch.SegmentedPolynomial(poly_o1, name="messages_o1")
+    self.tens_prod_o0 = FastSegmentedPolynomialFromUniform1dJit(poly_o0, name="messages_o0")
+    self.tens_prod_o1 = FastSegmentedPolynomialFromUniform1dJit(poly_o1, name="messages_o1")
     self.rank = rank
     self.num_o1_irreps = len(irreps_o1)
   def forward(self, z0_j, z1_j, dir_ij, dist_emb_ij):
@@ -82,9 +102,9 @@ class MessageContract(nn.Module):
     dist_emb_ij = self.dist_mlp(dist_emb_ij)
     sh_ij = self.sh(dir_ij)
     sh_dist_emb_ij = sh_ij[..., :, None]*dist_emb_ij[..., None, :] # (irrep, rank) ordering
-    z_j = torch.cat([z0_j, _flatten_vec(z1_j)], dim=-1)
-    z0_o, = self.tens_prod_o0([z_j, sh_dist_emb_ij])
-    z1_o, = self.tens_prod_o1([z_j, sh_dist_emb_ij])
+    z_j = torch.cat([z0_j, _flatten_irrep(1, z1_j)], dim=-1)
+    z0_o, = self.tens_prod_o0([z_j, sh_dist_emb_ij.reshape(-1, (1+3+5)*self.rank)])
+    z1_o, = self.tens_prod_o1([z_j, sh_dist_emb_ij.reshape(-1, (1+3+5)*self.rank)])
     z1_o = _transpose_irrep_muls_to_channels(self.rank, self.num_o1_irreps, 1, z1_o)
     ans = self.lin_0_o(z0_o), self.lin_1_o(z1_o)
     return ans
@@ -110,3 +130,79 @@ if __name__ == "__main__":
   check_symm(RotSymm(), msg_layer,
     [z_0, z_1, dir_ij, dist_emb_ij], ["0c", "1c", "1", "0c"], ["0c", "1c"])
   print("Done.")
+
+  print("BWD_PASS_CHECK")
+  # Check backward pass for etp_layer
+  z_0_orig = z_0.clone().detach().requires_grad_(True)
+  z_1_orig = z_1.clone().detach().requires_grad_(True)
+
+  # Forward pass
+  out_0, out_1 = etp_layer(z_0_orig, z_1_orig)
+  print(out_0.grad_fn.next_functions)
+
+  # Create random gradients for outputs
+  grad_out_0 = torch.randn_like(out_0)
+  grad_out_1 = torch.randn_like(out_1)
+
+  # Backward pass
+  loss =  (out_0 * grad_out_0).sum() + (out_1 * grad_out_1).sum()
+  loss.backward()
+  print(z_0_orig.grad)
+
+  # Store gradients
+  z_0_grad = z_0_orig.grad.clone()
+  z_1_grad = z_1_orig.grad.clone()
+
+  # Numerical check
+  epsilon = 1e-4
+  z_0_perturb = torch.randn_like(z_0) * epsilon
+  z_1_perturb = torch.randn_like(z_1) * epsilon
+
+  # Predicted change in output based on gradients
+  pred_change_0 = (z_0_perturb * z_0_grad).sum() + (z_1_perturb * z_1_grad).sum()
+
+  # Actual change in output by computing perturbed forward pass
+  z_0_new = z_0_orig.detach() + z_0_perturb
+  z_1_new = z_1_orig.detach() + z_1_perturb
+  out_0_new, out_1_new = etp_layer(z_0_new, z_1_new)
+  actual_change_0 = ((out_0_new - out_0) * grad_out_0).sum() + ((out_1_new - out_1) * grad_out_1).sum()
+
+  print(f"Backward pass check: predicted change = {pred_change_0.item():.6e}, actual change = {actual_change_0.item():.6e}")
+  print(f"Relative error: {abs(pred_change_0 - actual_change_0) / (abs(actual_change_0) + 1e-10):.6e}")
+
+  # Check symmetry properties of backward pass
+  print("Symmetry Checking Backward Pass...")
+
+  # Create symmetric gradient tensors for testing
+  grad_out_0 = torch.randn_like(out_0)
+  grad_out_1 = torch.randn_like(out_1)
+
+  # For ElementwiseTensorProducts
+  def etp_with_grad(z_0, z_1, grad_0, grad_1):
+    z_0 = z_0.clone().detach().requires_grad_(True)
+    z_1 = z_1.clone().detach().requires_grad_(True)
+    out_0, out_1 = etp_layer(z_0, z_1)
+    loss = (out_0 * grad_0).sum() + (out_1 * grad_1).sum()
+    loss.backward()
+    return z_0.grad, z_1.grad
+
+  check_symm(RotSymm(), etp_with_grad,
+    [z_0, z_1, grad_out_0, grad_out_1], ["0c", "1c", "0c", "1c"], ["0c", "1c"])
+
+  # For MessageContract
+  def msg_with_grad(z_0, z_1, dir_ij, dist_emb_ij, grad_0, grad_1):
+    z_0 = z_0.clone().detach().requires_grad_(True)
+    z_1 = z_1.clone().detach().requires_grad_(True)
+    dir_ij = dir_ij.clone().detach().requires_grad_(True)
+    dist_emb_ij = dist_emb_ij.clone().detach().requires_grad_(True)
+    out_0, out_1 = msg_layer(z_0, z_1, dir_ij, dist_emb_ij)
+    loss = (out_0 * grad_0).sum() + (out_1 * grad_1).sum()
+    loss.backward()
+    return z_0.grad, z_1.grad, dir_ij.grad, dist_emb_ij.grad
+
+  check_symm(RotSymm(), msg_with_grad,
+    [z_0, z_1, dir_ij, dist_emb_ij, grad_out_0, grad_out_1],
+    ["0c", "1c", "1", "0c", "0c", "1c"],
+    ["0c", "1c", "1", "0c"])
+
+  print("Backward Pass Symmetry Check Done.")
---
+++ managan/fast_tp_unif_1d_jit.py
@@ -0,0 +1,238 @@
+# Fast, low-overhead tensor uniform 1d. We're not supposed to be messing with this stuff, but NVIDIA provided
+# code (cuequivariance_ops_torch/tensor_product_uniform_1d_jit.py) with too much CPU overhead, so here we are.
+
+from itertools import accumulate
+from typing import Dict, List, Optional
+from collections import namedtuple
+
+import torch
+from torch import nn
+
+import cuequivariance as cueq
+# import the fast cuda kernel stuffs, plus useful definitions
+import cuequivariance_ops_torch._ext as ext
+from cuequivariance_ops_torch.tensor_product_uniform_1d_jit import BATCH_DIM_AUTO, BATCH_DIM_BATCHED, BATCH_DIM_INDEXED, BATCH_DIM_SHARED
+
+from managan.utils import must_be, prod
+
+
+def map_dtype(jit, dtype):
+  if dtype == torch.float32: return jit.Datatype.kFloat32
+  raise ValueError(f"Unknown dtype {dtype}")
+
+def map_buffer_dim(jit, o):
+  if o == 0: return jit.Dimension.kScalar
+  if o == 1: return jit.Dimension.kOneDimensional
+  raise ValueError(f"Unknown dimension {o}")
+
+
+class FastSegmentedPolynomialFromUniform1dJit(nn.Module):
+  def __init__(self,
+    polynomial: cueq.SegmentedPolynomial,
+    name: str = "segmented_polynomial",
+  ):
+    super().__init__()
+
+    operand_extent = None
+    for o in polynomial.operands:
+      torch._assert(
+        o.ndim in [0, 1],
+        "only 0 or 1 dimensional operands are supported by this method",
+      )
+      torch._assert(
+        all(len(s) == o.ndim for s in o.segments),
+        "all segments must have the same number of dimensions as the operand for this method",
+      )
+      torch._assert(
+        o.all_same_segment_shape(),
+        "all segments must have the same shape for this method",
+      )
+      if o.ndim == 1 and len(o.segments) > 0:
+        if operand_extent is None:
+          (operand_extent,) = o.segment_shape
+        else:
+          torch._assert(
+            (operand_extent,) == o.segment_shape,
+            "all operands must have the same extent for this method",
+          )
+    if operand_extent is None:
+      operand_extent = 1
+
+    for o, stp in polynomial.operations:
+      torch._assert(
+        stp.num_operands == len(o.buffers),
+        "the number of operands must match the number of buffers",
+      )
+      torch._assert(
+        stp.coefficient_subscripts == "", "the coefficients must be scalar"
+      )
+
+    self.num_inputs = polynomial.num_inputs
+    self.num_outputs = polynomial.num_outputs
+    self.name = name
+    self.operand_extent = operand_extent
+    self.buffer_dim = [o.ndim for o in polynomial.operands]
+    torch._assert(
+      all(buffer_dim in [0, 1] for buffer_dim in self.buffer_dim),
+      "buffer dimensions must be 0 or 1",
+    )
+    self.buffer_num_segments = [len(o.segments) for o in polynomial.operands]
+    self.dtypes = list(range(self.num_inputs)) + [-1]*polynomial.num_outputs
+    self.num_operations = len(polynomial.operations)
+    self.num_operands = [len(o.buffers) for o, stp in polynomial.operations]
+    self.operations = [b for o, stp in polynomial.operations for b in o.buffers]
+    self.num_paths = [stp.num_paths for o, stp in polynomial.operations]
+    self.path_indices_start = [0] + list(
+      accumulate([stp.num_paths * stp.num_operands for o, stp in polynomial.operations]))[:-1]
+    self.path_coefficients_start = [0] + list(
+      accumulate([stp.num_paths for o, stp in polynomial.operations]))[:-1]
+    self.path_indices = [i
+      for o, stp in polynomial.operations
+      for p in stp.paths for i in p.indices]
+    self.path_coefficients = [float(p.coefficients)
+      for o, stp in polynomial.operations
+      for p in stp.paths]
+    self.math_dtype = torch.float32
+    # precompute ops:
+    operation_index = 0
+    self.ops = []
+    for i in range(self.num_operations):
+      self.ops.append(self.operations[operation_index : operation_index + self.num_operands[i]])
+      operation_index += self.num_operands[i]
+    # construct the overall settings for this op:
+    self.uniform_1d_settings = Uniform1dSettings(
+      self.name, self.math_dtype, self.operand_extent, self.num_inputs, self.num_outputs, self.buffer_dim, self.buffer_num_segments,
+      self.ops, self.num_paths, self.path_indices_start, self.path_coefficients_start, self.path_indices, self.path_coefficients)
+  def forward(self, tensors: List[torch.Tensor]):
+    # check shapes
+    tensors = list(tensors)
+    rest = None
+    for i in range(len(tensors)):
+      assert tensors[i].dtype == self.math_dtype, "input has wrong dtype"
+      segments_dim = self.operand_extent*self.buffer_num_segments[i]
+      if rest is None:
+        *rest, must_be[segments_dim] = tensors[i].shape
+        batch_size = prod(rest)
+      else:
+        *must_be[rest], must_be[segments_dim] = tensors[i].shape
+      tensors[i] = tensors[i].contiguous().reshape(batch_size, segments_dim)
+    # apply kernel
+    ans = Uniform1dJit.apply(self.uniform_1d_settings, batch_size, *tensors)
+    return [
+      tens.reshape(*rest, -1)
+      for tens in ans
+    ]
+
+
+Uniform1dSettings = namedtuple("Uniform1dSettings", [
+  "name", "math_dtype", "operand_extent", "num_inputs", "num_outputs", "buffer_dim", "buffer_num_segments", "ops",
+  "num_paths", "path_indices_start", "path_coefficients_start", "path_indices", "path_coefficients"])
+
+
+
+class Uniform1dJit(torch.autograd.Function):
+  @staticmethod
+  def forward(ctx, settings:Uniform1dSettings, batch_size:int, *tensors):
+    assert len(tensors) == settings.num_inputs
+    # save data for bwd
+    ctx.save_for_backward(*tensors)
+    ctx.settings = settings
+    ctx.batch_size = batch_size
+    # make outputs list
+    outputs = []
+    for i in range(settings.num_inputs, settings.num_inputs + settings.num_outputs):
+      outputs.append(torch.empty(batch_size, settings.operand_extent*settings.buffer_num_segments[i],
+        dtype=settings.math_dtype, device=tensors[0].device))
+    # prep tensors
+    tensors = [t.contiguous() for t in tensors]
+    #tensors = [t.reshape(t.shape) for t in tensors] # this unwraps torch.Parameters into torch.Tensor
+    tensors = tensors + outputs
+    # prepare jit
+    jit = ext.tensor_product_uniform_1d_jit
+    # call jit
+    total_num_ops = settings.num_inputs + settings.num_outputs
+    jit.run(
+      settings.name,
+      map_dtype(jit, settings.math_dtype),
+      settings.operand_extent,
+      settings.num_inputs,
+      settings.num_outputs,
+      0, # no indexing
+      [map_buffer_dim(jit, b) for b in settings.buffer_dim],
+      settings.buffer_num_segments,
+      [jit.BatchDimension.kBatched] * total_num_ops, # everything must be batched
+      [-1] * total_num_ops, # no indexing
+      [map_dtype(jit, t.dtype) for t in tensors],
+      settings.ops,
+      settings.num_paths,
+      settings.path_indices_start,
+      settings.path_coefficients_start,
+      settings.path_indices,
+      settings.path_coefficients,
+      batch_size,
+      tensors,
+      torch.cuda.current_stream().cuda_stream
+    )
+    return tuple(outputs)
+  @staticmethod
+  def backward(ctx, *d_outputs):
+    # unpack useful data saved to ctx
+    *tensors, = ctx.saved_tensors
+    settings:Uniform1dSettings = ctx.settings
+    batch_size:int = ctx.batch_size
+    needs_input_grad = ctx.needs_input_grad[2:] # slice off settings and batch_size args
+    assert all(needs_input_grad), "holes in needs_input_grad not yet supported!"
+    # compute the new values needed for some of the settings
+    bwd_num_inputs = settings.num_inputs + settings.num_outputs
+    bwd_num_outputs = sum(1 if ng else 0 for ng in needs_input_grad)
+    bwd_buffer_dim = settings.buffer_dim + [
+      settings.buffer_dim[idx]
+      for idx, ng in enumerate(needs_input_grad) if ng
+    ]
+    bwd_buffer_num_segments = settings.buffer_num_segments + [
+      settings.buffer_num_segments[idx]
+      for idx, ng in enumerate(needs_input_grad) if ng
+    ]
+
+    # Compute a new list of ops! (see original code in tensor_product_uniform_1d_jit.py)
+    bwd_ops = []
+    bwd_num_paths = []
+    bwd_path_indices_start = []
+    bwd_path_coefficients_start = []
+    output_idx = bwd_num_inputs
+    for ng_idx, ng in enumerate(needs_input_grad):
+      if not ng: continue
+      # we want the derivative of input operand "idx"
+      # and store it into output operand bwd_num_input_operands + output_idx
+      # we have the gradients of the previous outputs in buffers orig_num_inputs ... orig_num_inputs + orig_num_outputs
+      #   i.e. we can keep them as is!
+      for ops_idx, op in enumerate(settings.ops):
+        # for a given operation, if it uses "idx" at a position k:
+        #   we replace "idx" at that position k with the output operand
+        #   we replace the output operand with its gradient
+        #   we add that to the list of operations
+        #   we also have to replicate num_paths, num_indices_start, num_coefficients_start
+        for op_idx, k in enumerate(op):
+          if k == ng_idx:
+            bwd_op = list(op)
+            bwd_op[op_idx] = output_idx
+            bwd_ops.append(bwd_op)
+            bwd_num_paths.append(settings.num_paths[ops_idx])
+            bwd_path_indices_start.append(settings.path_indices_start[ops_idx])
+            bwd_path_coefficients_start.append(
+              settings.path_coefficients_start[ops_idx]
+            )
+      output_idx += 1
+
+    bwd_path_indices = settings.path_indices
+    bwd_path_coefficients = settings.path_coefficients
+
+    # Recursively call this autograd fn so we can support higher derivatives
+    ans = Uniform1dJit.apply(
+      Uniform1dSettings(settings.name + "_bwd",
+        settings.math_dtype, settings.operand_extent, bwd_num_inputs, bwd_num_outputs, bwd_buffer_dim, bwd_buffer_num_segments,
+        bwd_ops, bwd_num_paths, bwd_path_indices_start, bwd_path_coefficients_start, bwd_path_indices, bwd_path_coefficients),
+      batch_size,
+      *tensors, *d_outputs
+    )
+    return None, None, *ans
